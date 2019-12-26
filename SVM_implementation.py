f4e967dcde3ae6532d2efdbe307cd9204c116364
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-12-26 17:50:36 +0200

    added visualization

--- SVM_implementation.py
+++ SVM_implementation.py
@@ -21,7 +21,7 @@ class support_vector_machine:
         transforms = [[1,1],
                       [-1,1],
                       [-1,-1],
-                      [1,1]]
+                      [1,-1]]
                                           
         all_data = []
         for yi in self.data:
@@ -33,10 +33,10 @@ class support_vector_machine:
         self.min_feature_value = min(all_data)
         all_data = None
         
-		# support vectors yi(xi.w+b) = 1
-		# choose a constant that is very near 1 to stop optimizing
-		
-		
+        # support vectors yi(xi.w+b) = 1
+        # choose a constant that is very near 1 to stop optimizing
+        
+        
         step_sizes = [self.max_feature_value * 0.1,     # big steps
                       self.max_feature_value * 0.01,    # smaller steps
                       # point of expense:
@@ -46,7 +46,7 @@ class support_vector_machine:
         b_range_multiple = 5    # does not have to be as presise as w
         
         # we don't need to take as small of steps 
-		# with b as we do w
+        # with b as we do w
         b_multiple = 5
        
         #saves a lot of processing; but less accurate
@@ -60,53 +60,92 @@ class support_vector_machine:
             
             while not optimized:
                 for b in np.arange(-1*(self.max_feature_value*b_range_multiple), 
-									self.max_feature_value*b_range_multiple,
-									step*b_multiple):		# like python range(); np.arange(first value in the range, last value, step size)
-					for transformation in transforms:
-						w_t = w*transformation
-						found_option = True
-						# This is the weakest link in the SVM funndamentally;
-						# SMO attempts to fix this a bit
-						# (run this calculation on all of the data to make sure it fits)
-						# Constraint function: yi*(xi.w+b) >= 1
-						for i in self.data:
-							for xi in self.data[i]:
-								yi = i
-								if not yi*(np.dot(w_t, xi) + b) >= 1:
-									found_option = False
-									# TODO should break here; as constraint function fails (so this yi does not work)
-						if found_option:
-							opt_dict[np.linalg.norm(w_t)] = [w_t, b]		# np.linalg.norm(w_t) - get the magnitude of the w_t vector
-			
-				# the two ws are identical, so choose one to compare
-				if w[0] < 0:
-					# not the best value, but works
-					optimized = True
-					print("Optimized a step.")
-				else: 
-					# w = [5, 5]
-					# step = 1
-					# w - step = [4, 4]
-					w = w - step
-					
-			norms = sorted([n for n in opt_dict])	# sort from lowest to highest
-			
-			# the w dictionary looks like this:
-			# ||w|| : [w, b]
-			# magnitude of w : [w,b]
-			# key : value
-			opt_choice = opt_dict[norms[0]]			# smallest norm 
-			
-			self.w = opt_choice[0]
-			self.b = opt_choice[1]
-			latest_optimum = opt_choice[0][0] + step*2
-		
+                                    self.max_feature_value*b_range_multiple,
+                                    step*b_multiple):        # like python range(); np.arange(first value in the range, last value, step size)
+                                    
+                    for transformation in transforms:
+                        w_t = w*transformation
+                        found_option = True
+                        # This is the weakest link in the SVM funndamentally;
+                        # SMO attempts to fix this a bit
+                        # (run this calculation on all of the data to make sure it fits)
+                        # Constraint function: yi*(xi.w+b) >= 1
+                        for i in self.data:
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t, xi) + b) >= 1:
+                                    found_option = False
+                                    # TODO should break here; as constraint function fails (so this yi does not work)
+                        if found_option:
+                            opt_dict[np.linalg.norm(w_t)] = [w_t, b]        # np.linalg.norm(w_t) - get the magnitude of the w_t vector
+            
+                # the two ws are identical, so choose one to compare
+                if w[0] < 0:
+                    # not the best value, but works
+                    optimized = True
+                    print("Optimized a step.")
+                else: 
+                    # w = [5, 5]
+                    # step = 1
+                    # w - step = [4, 4]
+                    w = w - step
+                    
+            norms = sorted([n for n in opt_dict])    # sort from lowest to highest
+            
+            # the w dictionary looks like this:
+            # ||w|| : [w, b]
+            # magnitude of w : [w,b]
+            # key : value
+            opt_choice = opt_dict[norms[0]]            # smallest norm 
+            
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+            latest_optimum = opt_choice[0][0] + step*2
+        
        
     def predict(self, features):
         # sign( x.w+b )
         classification = np.sign(np.dot(np.array(features), self.w) + self.b)
+        if classification != 0 and self.visualization: 
+            self.ax.scatter(features[0], features[1], s = 200, marker = "*", c = self.colors[classification])
         
         return classification
+
+    # not needed for the SVM; it is only for humans to see :)
+    def visualize(self):
+        [[self.ax.scatter(x[0], x[1], s = 100, color = self.colors[i]) for x in data_dictionary[i]] for i in data_dictionary]
+        
+        # hyperplane = x.w+b
+        # v = x.w+b
+        # positive support vector = 1
+        # negative support vector = -1
+        # decision boundary = 0
+        def hyperplane(x, w, b, v):            # v - the values we are seeking
+            return (-w[0]*x-b+v) / w[1]
+            
+        datarange = (self.min_feature_value*0.9, self.max_feature_value*1.1)
+        hyp_x_min = datarange[0]
+        hyp_x_max = datarange[1]
+        
+        # (w.x+b) = 1
+        # positive support vector hyperplane
+        psv1 = hyperplane(hyp_x_min, self.w, self.b, 1)        # psv1 - just a scalar value (y)
+        psv2 = hyperplane(hyp_x_max, self.w, self.b, 1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [psv1, psv2])
+        
+        # (w.x+b) = -1
+        # negative support vector hyperplane
+        nsv1 = hyperplane(hyp_x_min, self.w, self.b, -1)        # psv1 - just a scalar value (y)
+        nsv2 = hyperplane(hyp_x_max, self.w, self.b, -1)
+        self.ax.plot([hyp_x_min, hyp_x_max], [nsv1, nsv2])
+        
+        # (w.x+b) = 0
+        # decision boundary
+        db1 = hyperplane(hyp_x_min, self.w, self.b, 0)        # psv1 - just a scalar value (y)
+        db2 = hyperplane(hyp_x_max, self.w, self.b, 0)
+        self.ax.plot([hyp_x_min, hyp_x_max], [db1, db2])
+
+        plt.show()
 
 
 data_dictionary = {-1:np.array([[1, 7], 
@@ -116,4 +155,6 @@ data_dictionary = {-1:np.array([[1, 7],
                                 [6, -1], 
                                 [7, 3]])}
                                 
-                               
+svm = support_vector_machine()
+svm.fit(data = data_dictionary)
+svm.visualize()
